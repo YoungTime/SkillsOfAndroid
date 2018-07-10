1711c6e996f3ab5629836c0f22ec0f8e0a2abafe
duzeming <01378359@sf-express.com>
2018-07-10 17:12:07 +0800

    文件存储

--- app/src/main/java/com/example/a01378359/testapp/memory/MemoryActivity.java
+++ app/src/main/java/com/example/a01378359/testapp/memory/MemoryActivity.java
@@ -1,14 +1,29 @@
 package com.example.a01378359.testapp.memory;
 
 import android.content.SharedPreferences;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.a01378359.testapp.R;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 
 public class MemoryActivity extends AppCompatActivity {
     private EditText etMemory;
@@ -20,8 +35,10 @@ public class MemoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_memory);
         initView();
         setListener();
+        saveByFiles();
+//        saveByCache();
         if (getBySp()!=null){
-            etMemory.setText(getBySp());
+            etMemory.setText(getByCache());
         }
 
     }
@@ -58,5 +75,142 @@ public class MemoryActivity extends AppCompatActivity {
         // null 为默认值，如果没有数据则返回 null
         String memory = sharedPreferences.getString("memory",null);
         return memory;
+    }
+
+    // 文件存储
+    private String getByRaw(){
+        // 获取输入流
+        InputStream inputStream = this.getResources().openRawResource(R.raw.word);
+        // 使用 reader 一行一行读取，并添加到 StringBuffer 中拼接出来
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuffer buffer = new StringBuffer();
+        String s = null;
+        try {
+            while ((s = reader.readLine())!=null ){
+                buffer.append(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 打印出来
+        Log.i("raw:",buffer.toString());
+        return buffer.toString();
+    }
+
+    private String getByAssets(){
+
+        StringBuffer buffer = new StringBuffer();
+        // 获取输入流
+        try {
+            // 默认文件目录为 assets 根目录
+            InputStream inputStream = getAssets().open("word/word.txt");
+            // 使用 reader 一行一行读取，并添加到 StringBuffer 中拼接出来
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String s = null;
+            while ((s = reader.readLine())!=null ){
+                buffer.append(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.i("Assets:",buffer.toString());
+        return buffer.toString();
+    }
+
+    private void saveByFiles(){
+        try {
+            // 获取输出流，传入文件名和模式
+            OutputStream outputStream = this.openFileOutput("word",MODE_PRIVATE);
+            String s = "Hello World!";
+            byte[] bytes = s.getBytes("utf-8");
+            // 写入
+            outputStream.write(bytes);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private String getByFile(){
+        StringBuffer buffer = new StringBuffer();
+        try {
+            // 通过文件名读取
+            InputStream inputStream = this.openFileInput("word");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String s = null;
+            while((s = reader.readLine())!=null){
+                buffer.append(s);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        Log.i("Files:",buffer.toString());
+        return buffer.toString();
+    }
+
+    private void saveByCache(){
+        // 获取 cache 文件
+        File cacheFile = this.getCacheDir();
+        // 新文件，文件路径为 cache 文件夹的绝对路径里面的 wordCache 文件
+        File file = new File(cacheFile.getAbsolutePath() + "/wordCache");
+        try {
+            FileOutputStream outputStream = new FileOutputStream(file);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+            writer.write("Hello Cache");
+            writer.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    private String getByCache(){
+        File cacheFile = this.getCacheDir();
+        File file = new File(cacheFile.getAbsolutePath() + "/wordCache");
+        StringBuffer buffer = new StringBuffer();
+        try {
+            FileInputStream inputStream = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String s = null;
+            while ((s = reader.readLine())!=null){
+                buffer.append(s);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        Log.i("Cache:",buffer.toString());
+        return buffer.toString();
+    }
+
+    private void getByExternalFile(){
+        // 使用拓展文件,需要传入 String 类型的参数
+        // null 代表就是 file 目录下
+        File file1 = this.getExternalFilesDir(null);
+        // "word" 代表是 file/word/ 目录
+        File file2 = this.getExternalFilesDir("word");
+        // 使用静态常量，代表 file/Music/ 目录，有很多的常量，大家可以去 Environment 源码中看看
+        File file3 = this.getExternalFilesDir(Environment.DIRECTORY_MUSIC);
+        // 下面的操作和上面是一样的
+    }
+    private void getByExternalCache(){
+        File file = this.getExternalCacheDir();
+        // 获取公共存储空间的目录
+        File sFile = Environment.getExternalStorageDirectory();
+    }
+
+    private boolean isSdCardUsable(){
+        String state = Environment.getExternalStorageState();
+        if (state == Environment.MEDIA_MOUNTED){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
